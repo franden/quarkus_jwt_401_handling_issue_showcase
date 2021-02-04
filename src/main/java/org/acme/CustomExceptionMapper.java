112b2efd34dd3183f8dda2dbdcb0b5071749b897
Frank <mail@denisfrank.de>
2021-02-04 10:31:16 +0100

    io.quarkus.security.UnauthorizedException is used now

--- src/main/java/org/acme/CustomExceptionMapper.java
+++ src/main/java/org/acme/CustomExceptionMapper.java
@@ -1,5 +1,6 @@
 package org.acme;
 
+import io.quarkus.security.UnauthorizedException;
 import javax.annotation.Priority;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -7,11 +8,11 @@ import javax.ws.rs.ext.Provider;
 
 @Provider
 @Priority(1)
-public class CustomExceptionMapper implements ExceptionMapper<Exception> {
+public class CustomExceptionMapper implements ExceptionMapper<UnauthorizedException> {
 
     @Override
-    public Response toResponse(Exception e) {
-        System.out.println("CustomExceptionMapper was invoked " + e);
+    public Response toResponse(io.quarkus.security.UnauthorizedException e) {
+        System.out.println("CustomExceptionMapper UnauthorizedException was invoked " + e);
         return Response.serverError().build();
     }
 }
